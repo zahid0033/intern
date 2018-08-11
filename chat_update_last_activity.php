795c151f13eb9ccaeb3577d05f1724722b489df2
Zahid <zahidhossain0033@gmail.com>
2018-08-11 13:04:15 +0600

    chat added as a demo

--- chat_update_last_activity.php
+++ chat_update_last_activity.php
@@ -3,16 +3,17 @@
 require "config.php";
 session_start();
 
+$query = "";
 if(isset($_SESSION['un'])){
     $query = "update candidate set cLastActivity=now() WHERE cMail='$_SESSION[un]'";
-    mysqli_query($conn,$query);
+    
 }
 else if($_SESSION['en']){
     $query = "update employer set eLastActivity=now() WHERE eMail='$_SESSION[en]'";
-    mysqli_query($conn,$query);
+   
 }
 
-
+mysqli_query($conn,$query);
 
 
 
